91cf710b9b06886fe3d7d157d69a2c786e5c092c
+ <familymanjosh@gmail.com>
2023-04-03 21:19:28 -0700

    hi

--- src/food.js
+++ src/food.js
@@ -1,4 +1,4 @@
-const url = "https://www.themealdb.com/api/json/v2/"
+
 
 const searchform = document.getElementById("search-form")
 const searchInput = document.getElementById("search")
@@ -7,40 +7,22 @@ const menu = document.getElementById("container1")
 
 
 const searchBar = () => { 
-    fetch(`${url}${apiKey}+"/randomselection.php"`)
+    fetch(`${apiKey}search.php?s=${searchInput.value}`)
     .then(response => response.json())
     .then(meal => {
         console.log(meal)
             renderRandomMeal(meal)
-            foodDetails(meal)
     })
 }
 const renderRandomMeal = (meal) => {
-    const pTag = document.createElement("h1")
-    pTag.textContent = meal.strMeal
-    menu.append(pTag)
+    const li = document.createElement("li")
+    li.textContent = meal.strMeal
     searchBtn.addEventListener("submit", (e) => {
         e.preventDefault()
-        foodDetails()
 })
-const foodDetails = (meal) => {
-    const li = document.createElement("li")
-    li.forEach(ingredient => {
-        li.textContent = meal.strIngredient
-        menu.append(li)
-    });
-    const li2 = document.createElement("li")
-    li2.forEach(measure => {
-        li2.textContent = meal.strMeasure
-        menu.append(li2)
-    })
-    const li3 = document.createElement("li")
-    li3.forEach(measure => {
-        li3.textContent = meal.strMeal
-        menu.append(li3)
-    })
+menu.append(li)
 }
-}
+
 const init = () => {
     searchBar()
 }
