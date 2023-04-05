ea2695091ab50d8b59d85b60db886c4b00387f15
+ <familymanjosh@gmail.com>
2023-04-05 11:02:39 -0700

    added events and more java

--- src/food.js
+++ src/food.js
@@ -16,7 +16,7 @@ const filtersData = {
     "Random Meal": "random-meal"
 }
 
-let key = (filtersData) => {
+for (let key in filtersData) {
     let option = document.createElement("option");
     option.setAttribute('value', `${filtersData[key]}`);
   
@@ -58,7 +58,7 @@ filtersDropDown.addEventListener('change', (event) => {
         .then(mealObj => {
           console.log(mealObj);
           let allMeals = mealObj.meals;
-          cards.innerHTML = "";
+          sidebar.innerHTML = "";
           allMeals.forEach(meal => { 
             const ingredients = []
             for (let i = 1; i <= 20; i++) {
@@ -124,19 +124,19 @@ filtersDropDown.addEventListener('change', (event) => {
   }
   const renderLatest = (meal) => {
       latestBtn.addEventListener("click", (e) => {
-      const latestCard = document.createElement("div");
-      latestCard.classList.add("latestCard");
-      latestCard.innerHTML = `
-      <div class="latestCard-info">
+        const card = document.createElement("div");
+        card.classList.add("card")
+      card.innerHTML = `
+      <div class="card-info">
         <img id="img" src=${meal.strMealThumb}>
-        <h2 id="latestCard-name">${meal.strMeal}</h2>
-        <p id="latestCard-area">${meal.strArea}</p>
-        <p id="latestCard-catagory">${meal.strCategory}</p>
-        <p id="latestCard-ingredients">${"Ingredients: " + meal.strIngredient1 + ", " + meal.strIngredient2 + ", " + meal.strIngredient3 + ", " + meal.strIngredient4 + ", " + meal.strIngredient5 + ", " + meal.strIngredient6 + ", " + meal.strIngredient7 + ", " + meal.strIngredient8 + ", " + meal.strIngredient9 + ", " + meal.strIngredient10 + ", " + meal.strIngredient11 + ", " + meal.strIngredient12 + ", " + meal.strIngredient13 + ", " + meal.strIngredient14 + ", " + meal.strIngredient15 + ", " + meal.strIngredient16 + ", " + meal.strIngredient17 + ", " + meal.strIngredient18 + ", " + meal.strIngredient19 + ", " + meal.strIngredient20 }</p>
-        <p id="latestCard-instructions">${meal.strInstructions}</p>
-        <p id="latestCard-measure">${"Measurements: " + meal.strMeasure1 + ", " + meal.strMeasure2 + ", " + meal.strMeasure3 + ", " + meal.strMeasure4 + ", " + meal.strMeasure5 + ", " + meal.strMeasure6 + ", " + meal.strMeasure7 + ", " + meal.strMeasure8 + ", " + meal.strMeasure9 + ", " + meal.strMeasure10 + ", " + meal.strMeasure11 + ", " + meal.strMeasure12 + ", " + meal.strMeasure13 + ", " + meal.strMeasure14 + ", " + meal.strMeasure15 + ", " + meal.strMeasure16 + ", " + meal.strMeasure17 + ", " + meal.strMeasure18 + ", " + meal.strMeasure19 + ", " + meal.strMeasure20}</p>
+        <h2 id="card-name">${meal.strMeal}</h2>
+        <p id="card-area">${meal.strArea}</p>
+        <p id="card-catagory">${meal.strCategory}</p>
+        <p id="card-ingredients">${"Ingredients: " + meal.strIngredient1 + ", " + meal.strIngredient2 + ", " + meal.strIngredient3 + ", " + meal.strIngredient4 + ", " + meal.strIngredient5 + ", " + meal.strIngredient6 + ", " + meal.strIngredient7 + ", " + meal.strIngredient8 + ", " + meal.strIngredient9 + ", " + meal.strIngredient10 + ", " + meal.strIngredient11 + ", " + meal.strIngredient12 + ", " + meal.strIngredient13 + ", " + meal.strIngredient14 + ", " + meal.strIngredient15 + ", " + meal.strIngredient16 + ", " + meal.strIngredient17 + ", " + meal.strIngredient18 + ", " + meal.strIngredient19 + ", " + meal.strIngredient20 }</p>
+        <p id="card-instructions">${meal.strInstructions}</p>
+        <p id="card-measure">${"Measurements: " + meal.strMeasure1 + ", " + meal.strMeasure2 + ", " + meal.strMeasure3 + ", " + meal.strMeasure4 + ", " + meal.strMeasure5 + ", " + meal.strMeasure6 + ", " + meal.strMeasure7 + ", " + meal.strMeasure8 + ", " + meal.strMeasure9 + ", " + meal.strMeasure10 + ", " + meal.strMeasure11 + ", " + meal.strMeasure12 + ", " + meal.strMeasure13 + ", " + meal.strMeasure14 + ", " + meal.strMeasure15 + ", " + meal.strMeasure16 + ", " + meal.strMeasure17 + ", " + meal.strMeasure18 + ", " + meal.strMeasure19 + ", " + meal.strMeasure20}</p>
       </div>`
-      sidebar.append(latestCard)
+      sidebar.append(card)
       console.log(e.target)
   })
   }
@@ -153,15 +153,15 @@ filtersDropDown.addEventListener('change', (event) => {
   });
   }
   const renderAllIngredients = (ing) => {
-        const ingCard = document.createElement("div");
-        ingCard.classList.add("ingCard");
-        ingCard.innerHTML = `
-        <div class="ingCard-info">
-            <h2 id="ingCard-name">${ing.strIngredient}</h2>
-            <p id="ingCard-area">${ing.strDescription}</p>
+    const card = document.createElement("div");
+    card.classList.add("card")
+        card.innerHTML = `
+        <div class="card-info">
+            <h2 id="card-name">${ing.strIngredient}</h2>
+            <p id="card-area">${ing.strDescription}</p>
         </div>` 
         sidebar.innerHTML = "";
-        sidebar.append(ingCard)
+        sidebar.append(card)
     }
       
   const fetchCategories = () => {
@@ -177,14 +177,14 @@ filtersDropDown.addEventListener('change', (event) => {
   });
   }
   const renderAllCategories = (cat) => {
-        const catCard = document.createElement("div");
-        catCard.classList.add("catCard");
-        catCard.innerHTML = `
-        <div class="catCard-info">
-            <h2 id="catCard-name">${cat.strCategory}</h2>
+    const card = document.createElement("div");
+    card.classList.add("card")
+        card.innerHTML = `
+        <div class="card-info">
+            <h2 id="card-name">${cat.strCategory}</h2>
             </div>`
         sidebar.innerHTML = "";
-        sidebar.append(catCard)
+        sidebar.append(card)
     }
   const fetchAreas = () => {
   fetch(`${apiKey}list.php?a=list`)
@@ -199,14 +199,14 @@ filtersDropDown.addEventListener('change', (event) => {
   });
   }
   const renderAllAreas = (area) => {
-        const areaCard = document.createElement("div");
-        areaCard.classList.add("areaCard");
-        areaCard.innerHTML = `
-        <div class="areaCard-info">
-            <h2 id="areaCard-name">${area.strArea}</h2>
+    const card = document.createElement("div");
+    card.classList.add("card")
+        card.innerHTML = `
+        <div class="card-info">
+            <h2 id="card-name">${area.strArea}</h2>
             </div>`
             sidebar.innerHTML = "";
-        sidebar.append(areaCard)
+        sidebar.append(card)
     }
   const fetchRandom = () => {
   fetch(`${apiKey}random.php`)
@@ -233,20 +233,20 @@ filtersDropDown.addEventListener('change', (event) => {
   });
   }
 const renderRandom = (random) => {
-        const randomCard = document.createElement("div");
-        randomCard.classList.add("randomCard");
-        randomCard.innerHTML = `
-        <div class="randomCard-info">
+    const card = document.createElement("div");
+    card.classList.add("card")
+        card.innerHTML = `
+        <div class="card-info">
             <img id="img" src=${random.strMealThumb}>
-            <h2 id="randomCard-name">${random.strMeal}</h2>
-            <p id="randomCard-area">${random.strArea}</p>
-            <p id="randomCard-catagory">${random.strCategory}</p>
-            <p id="randomCard-ingredients">${"Ingredients: " + random.strIngredient1 + ", " + random.strIngredient2 + ", " + random.strIngredient3 + ", " + random.strIngredient4 + ", " + random.strIngredient5 + ", " + random.strIngredient6 + ", " + random.strIngredient7 + ", " + random.strIngredient8 + ", " + random.strIngredient9 + ", " + random.strIngredient10 + ", " + random.strIngredient11 + ", " + random.strIngredient12 + ", " + random.strIngredient13 + ", " + random.strIngredient14 + ", " + random.strIngredient15 + ", " + random.strIngredient16 + ", " + random.strIngredient17 + ", " + random.strIngredient18 + ", " + random.strIngredient19 + ", " + random.strIngredient20 }</p>
-            <p id="randomCard-instructions">${random.strInstructions}</p>
-            <p id="randomCard-measure">${"Measurements: " + random.strMeasure1 + ", " + random.strMeasure2 + ", " + random.strMeasure3 + ", " + random.strMeasure4 + ", " + random.strMeasure5 + ", " + random.strMeasure6 + ", " + random.strMeasure7 + ", " + random.strMeasure8 + ", " + random.strMeasure9 + ", " + random.strMeasure10 + ", " + random.strMeasure11 + ", " + random.strMeasure12 + ", " + random.strMeasure13 + ", " + random.strMeasure14 + ", " + random.strMeasure15 + ", " + random.strMeasure16 + ", " + random.strMeasure17 + ", " + random.strMeasure18 + ", " + random.strMeasure19 + ", " + random.strMeasure20}</p>  
+            <h2 id="card-name">${random.strMeal}</h2>
+            <p id="card-area">${random.strArea}</p>
+            <p id="card-catagory">${random.strCategory}</p>
+            <p id="card-ingredients">${"Ingredients: " + random.strIngredient1 + ", " + random.strIngredient2 + ", " + random.strIngredient3 + ", " + random.strIngredient4 + ", " + random.strIngredient5 + ", " + random.strIngredient6 + ", " + random.strIngredient7 + ", " + random.strIngredient8 + ", " + random.strIngredient9 + ", " + random.strIngredient10 + ", " + random.strIngredient11 + ", " + random.strIngredient12 + ", " + random.strIngredient13 + ", " + random.strIngredient14 + ", " + random.strIngredient15 + ", " + random.strIngredient16 + ", " + random.strIngredient17 + ", " + random.strIngredient18 + ", " + random.strIngredient19 + ", " + random.strIngredient20 }</p>
+            <p id="card-instructions">${random.strInstructions}</p>
+            <p id="card-measure">${"Measurements: " + random.strMeasure1 + ", " + random.strMeasure2 + ", " + random.strMeasure3 + ", " + random.strMeasure4 + ", " + random.strMeasure5 + ", " + random.strMeasure6 + ", " + random.strMeasure7 + ", " + random.strMeasure8 + ", " + random.strMeasure9 + ", " + random.strMeasure10 + ", " + random.strMeasure11 + ", " + random.strMeasure12 + ", " + random.strMeasure13 + ", " + random.strMeasure14 + ", " + random.strMeasure15 + ", " + random.strMeasure16 + ", " + random.strMeasure17 + ", " + random.strMeasure18 + ", " + random.strMeasure19 + ", " + random.strMeasure20}</p>  
             </div>`
             sidebar.innerHTML = "";
-        sidebar.append(randomCard)
+        sidebar.append(card)
     }
 
 const init = () => {
